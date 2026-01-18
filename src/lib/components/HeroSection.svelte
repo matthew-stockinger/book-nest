71130db428fcf570679dcaa32c667bd5cf9bfb0f
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-17 22:07:08 -0600

    complete 77

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import heroImage from '$lib/assets/hero.png';
+</script>
+
+<section class="default-margin hero">
+	<div class="hero-text">
+		<h1>Book Nest</h1>
+		<h3>Your personal book library</h3>
+		<h4 class="mt-l">
+			Create your very own digital library where you can keep track of every book you own, read and
+			love.
+		</h4>
+		<h4 class="mb-s">
+			Our app offers a beautifully designed, easy-to-use interface that makes managing your book
+			collection a joy.
+		</h4>
+		<button>Sign up</button>
+	</div>
+	<img src={heroImage} alt="" class="hero-image" />
+</section>
