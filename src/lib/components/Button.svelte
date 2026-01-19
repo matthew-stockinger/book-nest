aeef38a507a652b6126f4a2bc15c64de536d6de4
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-18 20:19:57 -0600

    complete 80

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -44,3 +44,32 @@
 		{@render children()}
 	</button>
 {/if}
+
+<style>
+	.btn {
+		padding: 12px 24px;
+		min-width: 230px;
+		text-align: center;
+		background-color: black;
+		border-radius: 12px;
+		color: white;
+		border: 1px solid white;
+		font-weight: normal;
+		font-size: 22px;
+	}
+
+	.btn-secondary {
+		background-color: white;
+		color: black;
+		border: 1px solid black;
+	}
+
+	.btn-danger {
+		background-color: rgb(136, 4, 4);
+	}
+
+	.btn-menu {
+		min-width: 150px;
+		padding: 8px 20px;
+	}
+</style>
