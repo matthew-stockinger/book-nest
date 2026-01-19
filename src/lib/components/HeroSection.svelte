3bcca3d257d0c7fcb6f8a0d20dfe22999b42a9f9
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-18 19:48:41 -0600

    complete 78

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -18,3 +18,25 @@
 	</div>
 	<img src={heroImage} alt="" class="hero-image" />
 </section>
+
+<style>
+	.hero {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-top: 100px;
+		margin-bottom: 80px;
+	}
+
+	.hero-image {
+		width: 40%;
+	}
+
+	.hero-text {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		width: 55%;
+		padding-right: 50px;
+	}
+</style>
