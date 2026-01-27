21397951048fc824f418a7f7b95061f8fa072b8b
Matthew Stockinger <matt@matthewstockinger.com>
2026-01-27 07:06:49 -0600

    8 min into 91

---
+++ src/lib/state/user-state.svelte.ts
@@ -0,0 +1,30 @@
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js';
+import { getContext, setContext } from 'svelte';
+
+interface UserStateProps {
+	session: Session | null;
+	supabase: SupabaseClient | null;
+	user: User | null;
+}
+
+export class UserState {
+	session = $state<Session | null>(null);
+	supabase = $state<SupabaseClient | null>(null);
+	user = $state<User | null>(null);
+
+	constructor(data: UserStateProps) {
+		this.updateState(data);
+	}
+
+	updateState(data: UserStateProps) {
+		this.session = data.session;
+		this.supabase = data.supabase;
+		this.user = data.user;
+	}
+}
+
+const USER_STATE_KEY = Symbol('USER_STATE');
+
+export function setUserState(data: UserStateProps) {
+	return setContext(USER_STATE_KEY, new UserState(data));
+}
