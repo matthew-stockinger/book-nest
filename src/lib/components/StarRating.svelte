16bc921f8ca3d529efb8d19906aad8b03a654913
Matthew Stockinger <matt@matthewstockinger.com>
2026-02-08 10:00:33 -0600

    complete 104 style StarRating comnponent

--- src/lib/components/StarRating.svelte
+++ src/lib/components/StarRating.svelte
@@ -42,3 +42,26 @@
 		{/each}
 	</div>
 </div>
+
+<style>
+  .rating-container {
+    display: inline-flex;
+    outline: none;
+
+  }
+
+  .star {
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 0;
+    font-size: 32px;
+    color: tranparent;
+  }
+
+  .star span {
+    display: inline-block;
+    color: var(--fill);
+  }
+</style>
