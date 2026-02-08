3e2ee9d20ccb762b2d71e255fadb1d8bd62f6168
Matthew Stockinger <matt@matthewstockinger.com>
2026-02-08 08:05:06 -0600

    complete 99 html for header of dashboard main section

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 	import { getUserState } from '$lib/state/user-state.svelte';
+	import Icon from '@iconify/svelte';
 
 	let userContext = getUserState();
-	let { allBooks } = $derived(userContext);
-
-	$inspect(allBooks);
+	let { userName } = $derived(userContext);
 </script>
 
-<ul>
-	{#each allBooks as book}
-		<li>{book.title}</li>
-	{/each}
-</ul>
+<div class="dashboard">
+	<div class="dashboard-header mb-m">
+		<a href="/private/scan-shelf" class="add-book">
+			<Icon icon="icons8:plus" width="72" height="72" />
+			<p>Add a book</p>
+		</a>
+		<div class="headline">
+			<h3 class="bold mb-xs">Welcome back, {userName}</h3>
+			<p>
+				There's nothing quite like the journey a good book can take you on. Have you discovered any
+				new favourites lately?
+			</p>
+		</div>
+	</div>
+
+	<!-- BookCategories -->
+</div>
