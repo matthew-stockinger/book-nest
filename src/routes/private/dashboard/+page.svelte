a5bbec349b4c5eb3d56e080a45b9333810c9a3be
Matthew Stockinger <matt@matthewstockinger.com>
2026-02-08 08:07:28 -0600

    complete 100 style dashboard header

--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -23,3 +23,28 @@
 
 	<!-- BookCategories -->
 </div>
+
+<style>
+	.dashboard-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-start;
+		width: 100%;
+	}
+
+	.add-book {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+	}
+
+	.add-book p {
+		margin-left: 8px;
+	}
+
+	.headline {
+		text-align: right;
+		max-width: 30%;
+		min-width: 300px;
+	}
+</style>
