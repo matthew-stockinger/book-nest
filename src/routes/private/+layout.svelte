f9232029fbee93fe094c4f46498b25058601e0ca
Matthew Stockinger <matt@matthewstockinger.com>
2026-02-08 07:55:45 -0600

    complete 97

---
+++ src/routes/private/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { SideNavigation } from '$components';
+	let { children } = $props();
+</script>
+
+<div class="auth-area">
+	<SideNavigation />
+	<div class="main-area">
+		{@render children()}
+	</div>
+</div>
