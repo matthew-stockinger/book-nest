29552495108409882552db607bc48aa0af371167
Matthew Stockinger <matt@matthewstockinger.com>
2026-02-21 16:07:57 -0600

    complete 118 OpenAI integration

--- src/routes/api/scan-shelf/+server.ts
+++ src/routes/api/scan-shelf/+server.ts
@@ -3,5 +3,23 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 export let POST: RequestHandler = async ({ request }) => {
 	let { base64 } = await request.json();
 
-	return json({ success: true });
+  // OpenAI integration would go here.  
+  // would need to pnpm add open AI's connector object, then send a prompt with base64 image to gpt-4-mini.
+  // it then sends back an array of book titles and authors
+  // here I'm returning a dummy array.
+
+  let bookArray = [
+    { booktitle: 'The Diary of a CEO', author: 'Steven Bartlett' },
+    { booktitle: 'Invisible Women', author: 'Caroline Criado Perez' },
+    { booktitle: 'Where Good Ideas Come From', author: 'Steven Johnson' },
+    { booktitle: 'The Narrows', author: 'Michael Connelly' },
+    { booktitle: 'The Drop', author: 'Michael Connelly' },
+    { booktitle: 'The Black Ice', author: 'Michael Connelly' },
+    { booktitle: 'Emotional Intelligence', author: 'Daniel Goleman' },
+    { booktitle: 'How to Read a Book', author: 'Mortimer J. Adler' },
+    { booktitle: 'The Unfair Advantage', author: 'Ash Ali' },
+    { booktitle: 'The Collingridge Dilemma', author: 'Jared Cohen' },
+  ];
+
+	return json({ bookArray });
 };
--- src/routes/private/scan-shelf/+page.svelte
+++ src/routes/private/scan-shelf/+page.svelte
@@ -5,13 +5,18 @@
 
 	let isLoading = $state(false);
 
+	interface OpenAiBook {
+		bookTitle: string;
+		author: string;
+	}
+
 	async function handleDrop(e: CustomEvent<any>) {
 		let { acceptedFiles } = e.detail;
 
 		if (acceptedFiles.length) {
 			isLoading = true;
 			let fileToSendToOpenAi = acceptedFiles[0];
-      let base64String = await convertFileToBase64(fileToSendToOpenAi);
+			let base64String = await convertFileToBase64(fileToSendToOpenAi);
 
 			try {
 				// don't fetch from openAI on the frontend because it would expose API key
@@ -24,10 +29,9 @@
 					body: JSON.stringify({ base64: base64String })
 				});
 
-				console.log(`response on the frontend: ${response.status}`);
-			} catch (error) {
-
-      }
+				let result = (await response.json()) as { bookArray: OpenAiBook[] };
+				console.log(result);
+			} catch (error) {}
 		}
 	}
 </script>
