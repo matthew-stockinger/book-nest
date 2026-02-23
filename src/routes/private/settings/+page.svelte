c5aa7e711a23dbdee068a5d597b8d89587fcff4b
Matthew Stockinger <matt@matthewstockinger.com>
2026-02-23 06:35:45 -0600

    complete 123 style settings page

--- src/routes/private/settings/+page.svelte
+++ src/routes/private/settings/+page.svelte
@@ -48,3 +48,29 @@
 		<h3>{averageRating}</h3>
 	</div>
 </div>
+
+<style>
+	.settings-page {
+		display: flex;
+		width: 100%;
+		justify-content: flex-start;
+		align-items: center;
+	}
+
+	.settings-container {
+		margin-right: 80px;
+
+	}
+
+	.settings-container input {
+		width: 100%;
+	}
+
+	.stats-container {
+		min-width: 25%;
+		border-radius: 12px;
+		padding: 8px 24px;
+		background-color: rgba(255, 255, 255, 0.5);
+		margin-bottom: 40px;
+	}
+</style>
